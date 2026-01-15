34e664f80fc1e5e1184205b11d3a537945cf6c88
tusu268-create <tushu268@gmail.com>
2026-01-15 14:44:49 +0530

    Update gemini_service.py

--- gemini_service.py
+++ gemini_service.py
@@ -1,8 +1,16 @@
 import json
 import os
+import google.generativeai as genai
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 json_path = os.path.join(BASE_DIR, "services.json")
 
 with open(json_path, "r") as f:
     services = json.load(f)
+
+genai.configure(api_key=services["GEMINI_API_KEY"])
+
+def get_gemini_response(prompt):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt)
+    return response.text
