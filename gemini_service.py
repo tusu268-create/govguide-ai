6ba3e4dad881b0b457ac77e6c4ff420953ed05d6
tusu268-create <tushu268@gmail.com>
2026-01-17 17:07:01 +0530

    Update gemini_service.py

--- gemini_service.py
+++ gemini_service.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(BASE_DIR, "config.json"), "r") as f:
     config = json.load(f)
 
-genai.configure(api_key=config["GEMINI_API_KEY"])
+genai.configure(api_key=config["AIzaSyBKRlO9GxqYPJxg7al4yUwZQ98t2ZgKxiU"])
 
 def get_gemini_response(prompt):
     model = genai.GenerativeModel("gemini-1.5-flash")
