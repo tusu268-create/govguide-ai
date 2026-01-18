2ba6ec11bed32373803d7d123fcc5f6cbbcc13f2
tusu268-create <tushu268@gmail.com>
2026-01-18 11:21:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import streamlit as st
 import json
 import os
 from gemini_service import get_gemini_response
+st.image("logo.png.jpeg", width=120)
 
+st.markdown(
+    "<h2 style='color:#8B6F47;'>GovGuide AI</h2>",
+    unsafe_allow_html=True
+)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(BASE_DIR, "services.json"), "r") as f:
