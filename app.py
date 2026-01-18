81b813dbc0ac380ac50cadaaa1a7e50c8a7133f5
tusu268-create <tushu268@gmail.com>
2026-01-18 17:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,12 +125,12 @@ st.markdown("""
 <div class="header">
 
     <div class="logo">
-        <img src="logo.png.jpeg">
+        <img src="logo.png">
     </div>
 
     <div class="center-text">
         <div class="satyamev">सत्यमेव जयते</div>
-        <div class="app-name">🏛️GovGuide AI</div>
+        <div class="app-name">GovGuide AI</div>
         <div class="subtext">Your trusted guide for Government Services</div>
         <div class="tagline">Towards a Corruption-Free, Informed India 🇮🇳</div>
         <div class="tiranga"></div>
